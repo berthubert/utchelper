d214d8103237503b9db59c2794c8512ab2c2b8c9
bert hubert <bert@hubertnet.nl>
2025-01-19 13:39:23 +0100

    spruce up output somewhat

--- utcparse.c
+++ utcparse.c
@@ -9,12 +9,13 @@ int main(int argc, char** argv)
     puts("Syntax: utcparse \"Wed, 31 Dec 1969 23:59:00\" [\"%a, %d %b %Y %H:%M:%S\"]");
     return EXIT_FAILURE;
   }
-  setlocale(LC_TIME, "");
+  setlocale(LC_TIME, ""); // respect the environment variables
+  
   struct tm tmt;
   if(!utcstr2epoch(argv[1], argc > 2 ? argv[2] : "%a, %d %b %Y %H:%M:%S", &tmt)) {
     char buffer[80];
-    strftime(buffer, sizeof(buffer), "%a, %d %b %Y %H:%M:%S", &tmt);
-    printf("UTC Time: %s\ntime_t:   %ld\n", buffer, timegm(&tmt));
+    if(strftime(buffer, sizeof(buffer), "%A, %e %B %Y %H:%M:%S, day of year %j", &tmt))
+      printf("UTC Time: %s\ntime_t:   %ld\n", buffer, timegm(&tmt));
   }
   else
     puts("Failed to parse time string");
